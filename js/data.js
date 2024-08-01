fd7047865a9604880ebf197a85acda395764f916
Zhou216 <Zhou_216@qq.com>
2024-08-02 00:23:21 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,315 @@
+let students=
+[
+    {
+        "name": "张佳薇",
+        "school": "莆田学院 - 临床医学",
+        "city": "莆田",
+        "province": "福建"
+    },
+    {
+        "name": "吴振坤",
+        "school": "安徽农业大学 - 生物育种科学",
+        "city": "合肥",
+        "province": "安徽"
+    },
+    {
+        "name": "陈静怡",
+        "school": "莆田学院 - 金融工程",
+        "city": "莆田",
+        "province": "福建"
+    },
+    {
+        "name": "林传鸿",
+        "school": "福建师范大学 - 数学与应用数学",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "陈金艳",
+        "school": "福建外语外贸学院 - 财务管理",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "郭思佳",
+        "school": "福州外语外贸学院 - 汉语国际教育",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "林腾镱",
+        "school": "四川大学锦江学院 - 通信工程",
+        "city": "眉山",
+        "province": "四川"
+    },
+    {
+        "name": "张伟祺",
+        "school": "华北水利水电大学 - 地质工程",
+        "city": "郑州",
+        "province": "河南"
+    },
+    {
+        "name": "郑源培",
+        "school": "西安文理学院 - 生物制药",
+        "city": "西安",
+        "province": "陕西"
+    },
+    {
+        "name": "丁载安",
+        "school": "集美大学 - 交通运输专业",
+        "city": "厦门",
+        "province": "福建"
+    },
+    {
+        "name": "彭梓涵",
+        "school": "长春工程学院 - 水利水电工程",
+        "city": "长春",
+        "province": "吉林"
+    },
+    {
+        "name": "蒋子瑶",
+        "school": "福建医科大学 - 基础医学",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "林硕",
+        "school": "吉林工商学院 - 工商管理",
+        "city": "长春",
+        "province": "吉林"
+    },
+    {
+        "name": "陈文斓",
+        "school": "集美大学 - 海洋渔业科学与技术",
+        "city": "厦门",
+        "province": "福建"
+    },
+    {
+        "name": "颜瑜芳",
+        "school": "莆田学院 - 临床医学",
+        "city": "莆田",
+        "province": "福建"
+    },
+    {
+        "name": "连政坪",
+        "school": "厦门医学院 - 海洋药学",
+        "city": "厦门",
+        "province": "福建"
+    },
+    {
+        "name": "陈华杰",
+        "school": "再奋斗",
+        "city": "泉州",
+        "province": "福建"
+    },
+    {
+        "name": "陈俊淮",
+        "school": "福建师范大学 - 食品科学与工程",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "李佳昕",
+        "school": "福建理工大学 - 信息与计算科学",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "赖诗薇",
+        "school": "福建中医药大学 - 临床医学",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "徐福坤",
+        "school": "闽南师范大学 - 应用化学",
+        "city": "漳州",
+        "province": "福建"
+    },
+    {
+        "name": "颜舒颖",
+        "school": "安徽财经大学 - 计算机",
+        "city": "蚌埠",
+        "province": "安徽"
+    },
+    {
+        "name": "许恬忻",
+        "school": "福建中医药大学 - 康复治疗学",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "吴嘉祺",
+        "school": "浙江农林大学 - 环境科学与工程",
+        "city": "杭州",
+        "province": "浙江"
+    },
+    {
+        "name": "连慧茹",
+        "school": "暂无",
+        "city": "泉州",
+        "province": "福建"
+    },
+    {
+        "name": "苏永强",
+        "school": "再奋斗",
+        "city": "泉州",
+        "province": "福建"
+    },
+    {
+        "name": "申林泰",
+        "school": "暂无",
+        "city": "泉州",
+        "province": "福建"
+    },
+    {
+        "name": "林玉晖",
+        "school": "福建师范大学协和学院 - 数字媒体技术",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "郑镕涛",
+        "school": "集美大学 - 海洋渔业科学与技术",
+        "city": "厦门",
+        "province": "福建"
+    },
+    {
+        "name": "李伟豪",
+        "school": "井冈山大学 - 电子信息科学与技术",
+        "city": "吉安",
+        "province": "江西"
+    },
+    {
+        "name": "曾思莹",
+        "school": "莆田学院 - 电气工程及其自动化",
+        "city": "莆田",
+        "province": "福建"
+    },
+    {
+        "name": "余建材",
+        "school": "福建中医药大学 - 康复治疗学",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "王丽敏",
+        "school": "福州外语外贸学院 - 汉语国际教育",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "郑雅丽",
+        "school": "福建警察学院 - 信息安全",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "林佳彤",
+        "school": "福建师范大学 - 英语",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "黄荣宏",
+        "school": "福建农林大学金山学院 - 土木工程",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "寇业炜",
+        "school": "厦门工学院 - 软件工程",
+        "city": "厦门",
+        "province": "福建"
+    },
+    {
+        "name": "许伟鹏",
+        "school": "闽南师范大学 - 软件工程",
+        "city": "漳州",
+        "province": "福建"
+    },
+    {
+        "name": "陈东赞",
+        "school": "集美大学 - 机械电子工程",
+        "city": "厦门",
+        "province": "福建"
+    },
+    {
+        "name": "赖业鑫",
+        "school": "福建医科大学 - 生物信息学",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "陈文渊",
+        "school": "福州大学至诚学院 - 数字媒体技术",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "黄泽雄",
+        "school": "福建理工大学 - 交通运输",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "周骏",
+        "school": "厦门工学院 - 软件工程",
+        "city": "厦门",
+        "province": "福建"
+    },
+    {
+        "name": "吴加福",
+        "school": "宁德师范学院（闽台合作） - 应用化学",
+        "city": "宁德",
+        "province": "福建"
+    },
+    {
+        "name": "许欣炜",
+        "school": "闽南师范大学 - 人力资源管理",
+        "city": "漳州",
+        "province": "福建"
+    },
+    {
+        "name": "何晨涵",
+        "school": "江苏海洋大学 - 生物技术",
+        "city": "连云港",
+        "province": "江苏"
+    },
+    {
+        "name": "苏祺化",
+        "school": "武夷学院 - 计算机科学与技术",
+        "city": "武夷山",
+        "province": "福建"
+    },
+    {
+        "name": "卢冰冰",
+        "school": "福州外语外贸学院 - 金融学",
+        "city": "福州",
+        "province": "福建"
+    },
+    {
+        "name": "陈启顺",
+        "school": "厦门理工学院 - 软件工程",
+        "city": "厦门",
+        "province": "福建"
+    },
+    {
+        "name": "欧加成",
+        "school": "江西理工大学 - 信息与计算科学",
+        "city": "赣州",
+        "province": "江西"
+    },
+    {
+        "name": "郑思睿",
+        "school": "再奋斗",
+        "city": "泉州",
+        "province": "福建"
+    },
+    {
+        "name": "颜敏津",
+        "school": "再奋斗",
+        "city": "泉州",
+        "province": "福建"
+    }
+]
